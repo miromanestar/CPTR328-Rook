c1f3e464c55ec7c825290621930666eb4355f199
Miro Manestar <miroimanestar@gmail.com>
2020-12-09 15:47:57 -0500

    Changed some stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,11 @@
     December 4, 2020
 */
 
+$(document).ready(function () {
+    stickyHeader();
+});
+
+/*
 //Global variables
 var currentUser = null;
 var users = null;
@@ -45,14 +50,14 @@ function login() {
                     setUserInfo(username);
                 });
             }).catch( (error) => {  console.error(`${ error.code }: ${ error.message }`); });
-    else
-        $('#auth-content').prepend(`
-        <div class="alert alert-danger" role="alert">
-            <p>
-                A user with the name <b><i>${ username }</i></b> is already online. Please choose another username.
-            </p>
-        </div>
-        `);
+        else
+            $('#auth-content').prepend(`
+            <div class="alert alert-danger" role="alert">
+                <p>
+                    A user with the name <b><i>${ username }</i></b> is already online. Please choose another username.
+                </p>
+            </div>
+            `);
     });
 }
 
@@ -99,4 +104,106 @@ function setUserInfo(username) {
         lastUpdate: Date.now(),
         type: null
     });
+}
+*/
+
+// -------------- PAGE LOADING FUNCTIONS --------------
+window.addEventListener('popstate', function (e) {
+    if (window.location.pathname === '/') {
+        loadContent(`home`, '', false);
+    } else {
+        loadContent(window.location.pathname.substr(1), '', false);
+    }
+});
+
+//When the page is loaded/refreshed, direct to correct page.
+function onFirstLoad() {
+    if (sessionStorage.getItem('redirect404') !== null) {
+        loadContent(sessionStorage.getItem('redirect404').substr(1));
+        sessionStorage.removeItem('redirect404');
+    } else {
+        loadContent('home');
+    }
+}
+
+function loadContent(selection, state, changeState) {
+    $('#page-content').fadeOut('fast', function () {
+        $('#page-content').load(`${window.location.origin}/pages/${selection}`, function (response, status) {
+            $('.navbar-collapse').collapse('hide');
+            if (status === 'success') {
+                loadPartials(); //Check for partials every time the page is reloaded, then finally run insertLightbox() when finished.
+                $('#page-content').fadeIn('fast');
+            }
+            if (status === 'error') {
+                loadContent('404'); //Possible infinite loop?
+                return;
+            }
+        });
+    });
+    
+    if (typeof changeState === 'undefined' && changeState !== false) {
+        if (selection === 'home') { //Instead of home having a /home.html url, display as base domain.
+            if (window.location.pathname !== '/') {
+                window.history.pushState(state, '', '/');
+                $('base').attr('href', '/')
+            }
+        } else if (selection !== '404' && selection !== window.location.pathname.substr(1)) { //Maintain page url despite 404
+            window.history.pushState(state, '', `/${selection}`);
+            $('base').attr('href', `${ location.origin }`)
+        }
+    }
+
+    //Make header link active based on URL
+    $('.nav-link').each(function () {
+        if ($(this).html().toLowerCase() === location.pathname.split('/')[1] || ( $(this).html().toLowerCase() === 'home' && location.pathname === '/' )) { //Highlight if on home page
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+        //Activate dropdowns
+        if ($(this).hasClass('dropdown-toggle')) {
+            if (location.pathname.split('/').length > 2 && $(this).prev().hasClass('active')) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        }
+    });
+
+    //Activate dropdown items
+    $('.dropdown-item').each(function () {
+        if ($(this).attr('onclick').split("'")[1] === location.pathname.substr(1)) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+}
+
+// -------------- MISCELLANEOUS FUNCTIONS --------------
+
+function stickyHeader() {
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top >= 50) {
+            $('.navigation').addClass('nav-bg');
+            $('header').addClass('divider-grey');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+            $('header').removeClass('divider-grey');
+        }
+    });
+}
+
+function loadPartials(callback) {
+    $('[partial]').each(function (i) {
+        $(this).load(`${ window.location.origin }/partials/${$(this).attr('partial')}`, function (response, status) {
+            $(this).contents().unwrap();
+            if (status === 'error') {
+                $(this).html(`Error loading partial: ${$(this).attr('partial')}`);
+            }
+        });
+    });
+    
+    if(callback)
+        callback();
 }
