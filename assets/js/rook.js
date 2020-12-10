4f4c592b10cf8fd25b67518a25695f9d7a5603d1
Miro Manestar <miroimanestar@gmail.com>
2020-12-09 19:57:23 -0500

    Added some validation

--- assets/js/rook.js
+++ assets/js/rook.js
@@ -1,7 +1,35 @@
 //Initialization
+var username = null;
+var room = null;
+var isHost = false;
+
+var playerList = null;
+
 $(document).ready( function() {
-    var username = window.history.state.username;
-    var room = window.history.state.room;
+    username = window.history.state.username;
+    room = window.history.state.room;
     
     $('#room_name').text(room);
+
+    //Figure out if host
+    firebase.database().ref(`/rooms/${ room }`).once('value').then( (data) => {
+        if (data.val().host === username)
+            isHost = true;
+    });
+
+    playerListUpdate();
+
+    if (isHost)
+        hostStuff();
 });
+
+function playerListUpdate() {
+    let playerData = firebase.database().ref(`/rooms/${ room }/players`);
+    playerData.on('value', (data) => {
+        playerList = data.val();
+    });
+}
+
+function hostStuff() {
+    let roomDB = firebase.database().ref(`/rooms/${ room }`);
+}
