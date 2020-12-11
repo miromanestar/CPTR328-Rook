9d4e7d38257d00c9a7eabc91dbba503201736b79
Miro Manestar <miroimanestar@gmail.com>
2020-12-10 21:57:22 -0500

    I don't even remember

--- assets/js/rook.js
+++ assets/js/rook.js
@@ -8,20 +8,53 @@ var playerList = null;
 $(document).ready( function() {
     username = window.history.state.username;
     room = window.history.state.room;
-    
+
     $('#room_name').text(room);
 
     //Figure out if host
-    firebase.database().ref(`/rooms/${ room }`).once('value').then( (data) => {
+    firebase.database().ref(`/rooms/${ room }`).on('value', (data) => {
         if (data.val().host === username)
             isHost = true;
+        else
+            isHost = false;
     });
 
+    $('#logout-btn').show();
     playerListUpdate();
 
     if (isHost)
         hostStuff();
+
+    setInterval(connectionChecks(), 20000);
 });
+
+
+// ---- GENERAL UPDATE STUFF FOR ALL CLIENTS
+
+function connectionChecks() {
+    firebase.database().ref(`/rooms/${ room }`).once('value').then( (data) => {
+        if (data.exists()) {
+            firebase.database().ref(`/rooms/${ room }`).update({ lastUpdate: Date.now(), players: { [username]: Date.now() } });
+            console.log('User connection verified.');
+        } else {
+            alert(`The room ${ room } was forcibly closed.`);
+            $('#auth-content').html(`
+                <div class="alert alert-info" role="alert">
+                        <p>
+                            Leaving room "${ room }".
+                        </p>
+                        <div class="spinner-border text-primary d-block mx-auto" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                </div>
+                `);
+
+                $('#auth-overlay').fadeIn('fast', function() {
+                    $('#auth-overlay').delay(500).fadeOut('fast', loadContent('home'));
+                });
+        }
+    });
+}
 
 function playerListUpdate() {
     let playerData = firebase.database().ref(`/rooms/${ room }/players`);
@@ -33,15 +66,18 @@ function playerListUpdate() {
 
         full = false;
         firebase.database().ref(`/rooms/${ room }`).once('value').then( (data) => {
-            console.log(numOfPlayers - data.val().max_players)
             if (numOfPlayers >= data.val().max_players)
                 full = true;
             else
                 full = false;
         }).then ( (data) => {
             //Update playerlist related data
-            firebase.database().ref(`/rooms/${ room }`).update({player_count: numOfPlayers, full: full});
-            displayPlayers();
+            if (numOfPlayers > 0) {
+                firebase.database().ref(`/rooms/${ room }`).update({player_count: numOfPlayers, full: full});
+                displayPlayers();
+            } else {
+                firebase.database().ref(`/rooms/${ room }`).remove();
+            }
         });
     });
 }
@@ -57,18 +93,53 @@ function displayPlayers() {
     }
 }
 
-function hostStuff() {
-    //Periodically delete players
-    //setInterval(function() { deletePlayers(); console.log('Purging players'); }, 60000);
+// ---- FUNCTIONS FOR EVERY USER ----
 
-    let roomDB = firebase.database().ref(`/rooms/${ room }`);
+function leaveRoom() {
+    let shouldLeave = confirm('Are you sure you want to leave the game?');
+    if (shouldLeave) {
+        firebase.database().ref(`/rooms/${ room }/players/${ username }`).remove();
+
+        if (isHost)
+            firebase.database().ref(`/rooms/${ room }`).update({ host: Object.keys(playerList)[0] })
+
+        $('#auth-content').html(`
+        <div class="alert alert-info" role="alert">
+                <p>
+                    Leaving room "${ room }".
+                </p>
+                <div class="spinner-border text-primary d-block mx-auto" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+        </div>
+        `);
+
+        $('#auth-overlay').fadeIn('fast', function() {
+            $('#auth-overlay').delay(500).fadeOut('fast', loadContent('home'));
+        });
+    }
 }
 
-// ---- HOST FUNCTIONS FOR DELETING DISCONNECTED PLAYERS
+
+// ---- FUNCTION FOR THe HOST ----
+
+function maintainHost() {
+
+}
+
+function hostStuff() {
+    //Periodically delete players
+    setInterval(function() { deletePlayers(); }, 60000);
+
+    let roomDB = firebase.database().ref(`/rooms/${ room }`);
+
+
+}
 
 function deletePlayers() {
     for (let player in playerList) {
         if (Date.now() - playerList[player].lastUpdate > 60000) {
+            console.log(`Purging players from room ${ room }`);
             firebase.database().ref(`/rooms/${ room }/players/${ player }`).remove();
         }
     }
